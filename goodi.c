f001ab1a7b7635d0cc0f1e9a69349fe59c111c11
Farzin <monseffarzin@gmail.com>
2021-04-12 18:03:19 +0430

    callback function for incoming data

--- goodi.c
+++ goodi.c
@@ -18,6 +18,11 @@ void curl_error_handler(CURL *handler, int res)
 	exit(2);
 }
 
+size_t write_callback(void *ptr, size_t size, size_t nmemb, void *userdata)
+{
+	return size * nmemb;
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc == 1)
@@ -37,6 +42,10 @@ int main(int argc, char *argv[])
 	strcat(url, argv[1]);
 
 	res = curl_easy_setopt(handler, CURLOPT_URL, url);
+	if (res != CURLE_OK)
+		curl_error_handler(handler, res);
+
+	res = curl_easy_setopt(handler, CURLOPT_WRITEFUNCTION, write_callback);
 	if (res != CURLE_OK)
 		curl_error_handler(handler, res);
 
